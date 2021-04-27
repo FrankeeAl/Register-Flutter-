85b59b05a1b3b441fa66889d2f3b2be66aed921c
Frankee Al <51074133+FrankeeAl@users.noreply.github.com>
2021-04-27 16:16:15 +0800

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:garreta_google_maps/screens/homeScreen.dart';
+import 'package:garreta_google_maps/screens/mapScreen.dart';
+import 'package:provider/provider.dart';
+import 'blocs/application_bloc.dart';
+import 'blocs/bloc.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => ApplicationBloc(),
+      //create: (context) => ProviderMaps(),
+
+      child: MaterialApp(
+        title: 'Local Map',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        // home: MapsScreen(),
+        home: HomeScreen(),
+      ),
+    );
+  }
+}
